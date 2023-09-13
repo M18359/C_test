ac351b048e600cc457118de91de6e0549abaaeb3
M18359 <1835914853Qqq.com>
2023-09-13 20:33:20 +0800

    9.13更新

--- test_9.9/test_9.9/mian.c
+++ test_9.9/test_9.9/mian.c
@@ -2,24 +2,136 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
-int my_strlen(const char* str)
-{
-	int count = 0;
-	assert(str);
-	while (*str++)
-	{
-		count++;
-	}
-	return count;
-}
+
+//喝汽水问题
 int main()
 {
-	char arr[] = "yes right";
-	int len = my_strlen(arr);
-	printf("%d", len);
+	int money = 0;
+	scanf("%d", &money);
+	int empty = 0;
+	int total = money;
+	empty = money;
+	//置换
+	while (empty >=2)
+	{
+		total += empty / 2;
+		empty -= empty / 2;
+
+	}
+	printf("%d", total);
 	return 0;
 }
+
+
+//打印菱形
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	//上半部分line/2+1
+//	int i = 0;//行
+//	for (i = 0; i < (line / 2 + 1); i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < (line / 2 - i); j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < (2 * i + 1); j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	//下半部分line/2
+//	for (i = 0; i < line / 2; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < (i + 1); j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2*(line/2-i)-1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//打印水仙花数
+//int main()
+//{
+//	int n = 0;
+//	for (n = 0; n < 100001; n++)
+//	{
+//		int s = 0;
+//		int tmp = n;
+//		//先确定有几位数-->s
+//		while (tmp)//123  12  1 
+//		{
+//			s++;
+//			tmp /= 10;
+//		}
+//		//判断是否为
+//		int sum = 0;
+//		tmp = n;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10,s);
+//			tmp /= 10;
+//		}
+//		if (n == sum)
+//			printf("%d ", n);
+//
+//	}
+//	return 0;
+//
+//}
+
+//a+aa+aaa+aaaa+aaaaa前n项之和
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d %d", &a,&n);
+//	int sum = 0;
+//	int i = 0;
+//	int k = 0;
+//
+//	for (i = 0; i < n; i++)
+//	{
+//		//a    sum+ a0+a   sum+a00+aa
+//		k += a * pow(10, i);
+//		sum += k;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str);
+//	while (*str++)
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "yes right";
+//	int len = my_strlen(arr);
+//	printf("%d", len);
+//	return 0;
+//}
+
+
 ////返回值设置为char*  是为了实现后续可以链式访问，作为其他函数的参数
 ////char* 返回的应该是dest的起始地址
 //char* my_strcpy(char* dest,const char* str)//防止输入反
