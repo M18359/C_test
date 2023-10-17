8050e82153d17f77a37964ea115baf5775e42ea4
M18359 <1835914853Qqq.com>
2023-10-17 17:07:31 +0800

    函数指针相关10.17

--- test10.13/test10.13/main.c
+++ test10.13/test10.13/main.c
@@ -90,6 +90,114 @@
 
 //函数指针用途
 //写一个计算器
+//void menu()
+//{
+//	printf("**********************\n");
+//	printf("*****1.add 2.sub******\n");
+//	printf("*****3.mul 4.div******\n");
+//	printf("*****   0.exit  ******\n");
+//	printf("**********************\n");
+//
+//}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+////通过函数指针把冗余部分中不同代码抽离出来
+//void calc(int(*pf)(int, int))
+//{
+//	
+//	int x = 0;
+//	int y = 0;
+//	int ret = 0;
+//	printf("请输入两个操作数：");
+//	scanf("%d %d", &x, &y);
+//	ret = pf(x, y);
+//	printf("%d\n", ret);
+//}
+//int main()
+//{
+//	int input = 0;
+//
+//	do
+//	{
+//		menu();
+//		printf("请选择：>");
+//		scanf("%d", &input);
+//		
+//
+//		switch (input)
+//		{
+//		case 1:
+//			calc(Add);
+//			break;
+//		case 2:
+//			calc(Sub);
+//
+//			break;
+//		case 3:
+//			calc(Mul);
+//
+//			break;
+//		case 4:
+//			calc(Div);
+//
+//			break;
+//		case 0:
+//			printf("退出计算器\n");
+//			break;
+//		default:
+//			printf("输入有误，请重新输入\n");
+//
+//			break;
+//		}
+//	} while (input);
+//
+//	return;
+//}
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//int main()
+//{
+//	int (*arr[4])(int, int) = { Add,Sub,Mul,Div };
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		int ret = arr[i](9, 3);
+//		printf("%d\n", ret);
+//	}
+//	return 0;
+//}
+
+
+//函数指针数组改进计算器
 void menu()
 {
 	printf("**********************\n");
@@ -130,38 +238,29 @@ void calc(int(*pf)(int, int))
 int main()
 {
 	int input = 0;
-
+	//函数指针数组存放函数名，方便增减计算器功能
+	int (*pArr[5])(int, int) = { 0,Add,Sub,Mul,Div };
 	do
 	{
 		menu();
 		printf("请选择：>");
 		scanf("%d", &input);
-		
-
-		switch (input)
+		if (input == 0)
 		{
-		case 1:
-			calc(Add);
-			break;
-		case 2:
-			calc(Sub);
-
-			break;
-		case 3:
-			calc(Mul);
-
-			break;
-		case 4:
-			calc(Div);
-
-			break;
-		case 0:
 			printf("退出计算器\n");
-			break;
-		default:
-			printf("输入有误，请重新输入\n");
-
-			break;
+		}
+		else if (input >= 1 && input <= 4)
+		{
+			int x = 0;
+			int y = 0;
+			printf("请输入两个操作数：>");
+			scanf("%d %d", &x, &y);
+			int ret=pArr[input](x, y);
+			printf("%d\n", ret);
+		}
+		else
+		{
+			printf("输入有误，请重新输入：\n");
 		}
 	} while (input);
 
